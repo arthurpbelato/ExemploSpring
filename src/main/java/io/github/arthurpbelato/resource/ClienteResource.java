adacdfb621946fe6eecaf81cf302f46a78459fa5
arthurpbelato <belatoarthur@outlook.com>
2020-10-22 09:45:14 -0300

    busca com filtros

--- src/main/java/io/github/arthurpbelato/resource/ClienteResource.java
+++ src/main/java/io/github/arthurpbelato/resource/ClienteResource.java
@@ -5,6 +5,8 @@ import io.github.arthurpbelato.repository.ClienteRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +26,12 @@ public class ClienteResource {
 
     @Autowired
     private ClienteRepository clienteRepository;
-
+    /*
     @GetMapping
     public List<Cliente> listar(){
         return clienteRepository.findAll();
     }
-
+    */
     @PostMapping
     public ResponseEntity<Cliente> cadastrarCliente(@RequestBody @Valid Cliente cliente, HttpServletResponse response){
         Cliente clienteSalvo = clienteRepository.save(cliente);
@@ -62,6 +64,18 @@ public class ClienteResource {
         BeanUtils.copyProperties(cliente, clienteExistente, "codigo");
         clienteRepository.save(clienteExistente);
         return ResponseEntity.ok(clienteExistente);
+    }
+
+    @GetMapping
+    public ResponseEntity bucarPorFiltro(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clienteRepository.findAll(example);
+        return ResponseEntity.ok(lista);
+
     }
 
 }
